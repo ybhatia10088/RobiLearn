e89fbfd28b90bb5d253fcdba5b812e1a25947445
ybhatia10088 <ybhatia2@wisc.edu>
2025-06-02 21:51:17 +0530

    Updated SimulatorPage.tsx

--- src/pages/SimulatorPage.tsx
+++ src/pages/SimulatorPage.tsx
@@ -93,8 +93,8 @@ const SimulatorPage: React.FC = () => {
                               : 'bg-dark-600'
                           }`}>
                             {objective.completed && (
-                              <svg width="12" height="12" viewBox="0 0 12 12" fill="none">
-                                <path d="M10 3L4.5 8.5L2 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                              <svg width="12\" height="12\" viewBox="0 0 12 12\" fill="none">
+                                <path d="M10 3L4.5 8.5L2 6\" stroke="currentColor\" strokeWidth="2\" strokeLinecap="round\" strokeLinejoin="round"/>
                               </svg>
                             )}
                           </div>
