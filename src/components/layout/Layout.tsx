f12304076b51642585b762d960f4523bd841935d
ybhatia10088 <ybhatia2@wisc.edu>
2025-06-02 21:45:48 +0530

    Fix layout spacing and content overlap

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,15 +8,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main 
-        className="flex-1 overflow-hidden"
-        style={{ 
-          height: `calc(100vh - var(--header-height) - var(--footer-height))` 
-        }}
-      >
-        <div className="container mx-auto h-full p-4">
+      <main className="flex-1 overflow-y-auto pt-[var(--header-height)] pb-8">
+        <div className="container mx-auto px-4">
           {children}
         </div>
       </main>
