2abd971d5c17432a51fc9608c3e6eccf5bfe10b4
ybhatia10088 <ybhatia2@wisc.edu>
2025-06-02 22:35:48 +0530

    Updated SceneContainer.tsx

--- src/components/simulator/SceneContainer.tsx
+++ src/components/simulator/SceneContainer.tsx
@@ -565,7 +565,7 @@ const SceneContainer: React.FC = () => {
         
         {/* Fog */}
         {config.fog && (
-          <fog attach="fog" args={[config.fog.color, config.fog.near, config.fog.far]} />
+          <fog attach="fog\" args={[config.fog.color, config.fog.near, config.fog.far]} />
         )}
         
         {/* Advanced lighting setup */}
